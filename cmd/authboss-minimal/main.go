bf2e4bf1dae21a5669f28c21eda68d70bce4bf17
Michael Lynch <git@mtlynch.io>
2023-06-03 12:35:04 -0400

    Adding user auth

--- cmd/authboss-minimal/main.go
+++ cmd/authboss-minimal/main.go
@@ -2,13 +2,18 @@ package main
 
 import (
 	"context"
+	"encoding/base64"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/gorilla/mux"
+	"github.com/gorilla/sessions"
+	abclientstate "github.com/volatiletech/authboss-clientstate"
 	"github.com/volatiletech/authboss/v3"
+	_ "github.com/volatiletech/authboss/v3/auth"
 	"github.com/volatiletech/authboss/v3/defaults"
 )
 
@@ -58,22 +63,37 @@ func main() {
 
 	ab := authboss.New()
 
-	var myDatabaseImplementation authboss.ServerStorer
-	var mySessionImplementation authboss.ClientStateReadWriter
-	var myCookieImplementation authboss.ClientStateReadWriter
-
-	myDatabaseImplementation = inMemoryStore{}
-
-	ab.Config.Storage.Server = myDatabaseImplementation
-	ab.Config.Storage.SessionState = mySessionImplementation
-	ab.Config.Storage.CookieState = myCookieImplementation
-
-	ab.Config.Paths.Mount = "/authboss"
-	ab.Config.Paths.RootURL = fmt.Sprintf("http://0.0.0.0:%s", port)
-
-	readJson := false
+	readJson := true
 	useUsername := false
 	defaults.SetCore(&ab.Config, readJson, useUsername)
+
+	ab.Config.Core.ViewRenderer = defaults.JSONRenderer{}
+
+	cookieStoreKey, err := base64.StdEncoding.DecodeString(`NpEPi8pEjKVjLGJ6kYCS+VTCzi6BUuDzU0wrwXyf5uDPArtlofn2AG6aTMiPmN3C909rsEWMNqJqhIVPGP3Exg==`)
+	if err != nil {
+		panic(err)
+	}
+
+	cookieStore := abclientstate.NewCookieStorer(cookieStoreKey, nil)
+	cookieStore.HTTPOnly = false
+	cookieStore.Secure = false
+	sessionStoreKey, err := base64.StdEncoding.DecodeString(`AbfYwmmt8UCwUuhd9qvfNA9UCuN1cVcKJN1ofbiky6xCyyBj20whe40rJa3Su0WOWLWcPpO1taqJdsEI/65+JA==`)
+	if err != nil {
+		panic(err)
+	}
+	const sessionCookieName = "ab_blog"
+	sessionStore := abclientstate.NewSessionStorer(sessionCookieName, sessionStoreKey, nil)
+	cstore := sessionStore.Store.(*sessions.CookieStore)
+	cstore.Options.HttpOnly = false
+	cstore.Options.Secure = false
+	cstore.MaxAge(int((30 * 24 * time.Hour) / time.Second))
+
+	ab.Config.Storage.Server = inMemoryStore{}
+	ab.Config.Storage.SessionState = sessionStore
+	ab.Config.Storage.CookieState = cookieStore
+
+	ab.Config.Paths.Mount = "/auth"
+	ab.Config.Paths.RootURL = fmt.Sprintf("http://0.0.0.0:%s", port)
 
 	if err := ab.Init(); err != nil {
 		panic(err)
@@ -82,9 +102,14 @@ func main() {
 	router := mux.NewRouter()
 	router.Use(ab.LoadClientStateMiddleware)
 
+	router.PathPrefix(ab.Config.Paths.Mount + "/login").Handler(http.StripPrefix(ab.Config.Paths.Mount, http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("we're in the authboss subrouter!")
+		log.Printf("request=%s", r.URL.Path)
+		ab.Config.Core.Router.ServeHTTP(w, r)
+	}))).Methods(http.MethodPost)
+
 	views := router.PathPrefix("/").Subrouter()
 	views.HandleFunc("/", publicHello)
-	views.Handle(ab.Config.Paths.Mount, http.StripPrefix(ab.Config.Paths.Mount, ab.Config.Core.Router))
 
 	authenticatedViews := router.PathPrefix("/").Subrouter()
 	authenticatedViews.Use(authboss.Middleware2(ab, authboss.RequireNone, authboss.RespondUnauthorized))
